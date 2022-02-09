5be975edfa7e773695fc886ef868e2d1270d04e4
giapdz <56904888+giapdz@users.noreply.github.com>
2022-02-09 17:45:23 +0700

    upload code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
+import { DatabaseModule } from './config/database/database.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -5,14 +5,14 @@ import {
   Get,
   Param,
   Post,
-  Put,
+  Patch,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/createPost.dto';
 import { UpdatePostDto } from './dto/updatePost.dto';
 
 @Controller('posts')
-export default class PostsController {
+export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
@@ -30,11 +30,10 @@ export default class PostsController {
     return this.postsService.createPost(post);
   }
 
-  @Put(':id')
-  async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
-    return this.postsService.replacePost(Number(id), post);
+  @Patch(':id')
+  async updatePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+    return this.postsService.updatePost(Number(id), post);
   }
-
   @Delete(':id')
   async deletePost(@Param('id') id: string) {
     this.postsService.deletePost(Number(id));
